20c359ee80d32ac89a3812892c42f2f763821acc
yangWs29 <781958371@qq.com>
2024-01-04 18:03:54 +0800

    添加跳转链接

--- explorer/src/app/page.tsx
+++ explorer/src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Space } from 'antd'
 import Link from 'next/link'
 
 const RootPage: React.FC = () => {
   return (
     <Card title="explorer display">
-      <Link href={'/path'}>/path</Link>
+      <Space direction="vertical">
+        <Link href={'/path'}>/path</Link>
+        <Link href={'/q-bittorrent'}>/qBittorrent</Link>
+      </Space>
     </Card>
   )
 }
